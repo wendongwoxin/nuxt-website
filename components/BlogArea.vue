f591b74889ce47faef1759b6fbca5055b9ed4068
wujinwen <wujinwen@leinao.ai>
2022-10-12 20:40:24 +0800

    1

--- components/BlogArea.vue
+++ components/BlogArea.vue
@@ -7,34 +7,36 @@
           <div class="blog_left_sidebar">
             <article class="blog_item" v-for="item in blogs" :key="item.id">
               <div class="blog_item_img">
-                <img :src="item.img" alt="" class="card-img rounded-0" />
+                <img :src="item.imageUrl" alt="" class="card-img rounded-0" />
                 <a :href="item.blogDetail" class="blog_item_date">
-                  <h3>{{ item.date }}</h3>
+                  <h3>{{ item.day }}</h3>
                   <p>{{ item.month }}</p>
                 </a>
               </div>
               <div class="blog_details">
                 <a class="d-inline-block" href="item.blogDetail">
-                  <h2>{{ item.title }}</h2>
+                  <h2>{{ item.blogName }}</h2>
                 </a>
                 <p>{{ item.description }}</p>
                 <ul class="blog-info-link">
-                  <li>
+                  <li class="hello">
                     <a href="javascript(0)">
-                      <i class="fa fa-user">{{ item.type }}</i>
+                      <i class="fa fa-user"></i>
+                      {{ item.keyword }}
                     </a>
                   </li>
                   <li>
                     <a href="javascript(0)">
                       <i class="fa fa-comments">
-                        {{ item.comments }} Comments</i
+                       </i
                       >
+                      {{ item.commentCount }}  Comments
                     </a>
                   </li>
                 </ul>
               </div>
             </article>
-            <n-pagination v-model:page="page" :page-count="total" />
+            <n-pagination v-model:page="page" :item-count="total" :page-size="5"/>
           </div>
         </div>
         <!-- search content -->
@@ -47,6 +49,32 @@
 <script lang="ts" setup>
 import { NPagination } from "naive-ui";
 const blogs = ref([]);
-const page = ref(5);
-const total = ref(0)
+const page = ref(1);
+const total = ref(0);
+const getBlogs = async () => {
+  try {
+    await useFetch(
+      () =>
+        `http://106.14.207.110:8081/blog/query?pageSize=5&currentPage=${page.value}`,
+      {
+        onResponse({ request, response, options }) {
+          // Process the response data
+          const data = response._data
+          if (data.status === 200) {
+            blogs.value = data.data.list;
+            total.value = data.data.total ;
+            blogs.value.map(item => {{
+              item.imageUrl = new URL(`../assets/img/blog/${item.image}`, import.meta.url).href
+            }})
+          }
+          return response._data;
+        },
+      }
+    );
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+getBlogs();
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: ["animate.css/animate.min.css", '~/assets/css/index.scss'],
+  css: ["animate.css/animate.min.css", "~/assets/css/index.scss"],
+  vite: {
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8081',
+          ws: true,
+          changeOrigin: true,
+        }
+      }
+    }
+  }
 });
